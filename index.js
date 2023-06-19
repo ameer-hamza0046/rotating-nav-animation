1a2a3fc8d1de86dd4147349f40a36ac691c56c92
ameer-hamza <ameer.hamza0046@gmail.com>
2023-06-19 21:24:36 +0530

    completed website

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const [open_btn, close_btn] = document.querySelectorAll(".circle-container button")
+const [open_btn, close_btn] = document.querySelectorAll(".circle-container button");
 const circle_container = document.querySelector(".circle-container");
+const container = document.querySelector(".container");
+const nav = document.querySelector("#mynav")
 
 open_btn.addEventListener("click", () => {
   circle_container.classList.add("rotated");
-})
+  container.classList.add("rotated");
+  nav.classList.add("rotated");
+});
 
 close_btn.addEventListener("click", () => {
   circle_container.classList.remove("rotated");
-})
+  container.classList.remove("rotated");
+  nav.classList.remove("rotated");
+});
